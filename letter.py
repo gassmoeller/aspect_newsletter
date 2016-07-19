2d7c2c35dd5db28a9e2f421df453019ce16f2d95
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2016-07-19 15:09:09 -0500

    Save last report date

--- letter.py
+++ letter.py
@@ -4,6 +4,7 @@ import os
 import requests
 import datetime
 import smtplib
+from unidecode import unidecode
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -14,13 +15,13 @@ from time import strftime
 
 def get_headers():
     # Create the header of the message (a plain-text and an HTML version).
-    text_header = "Hello everyone!\n\nThis is the biweekly ASPECT newsletter #" + str(number) + ".\nIt automatically reports recently merged features and discussions about the ASPECT mantle convection code.\n\n"
+    text_header = "Hello everyone!\n\nThis is ASPECT newsletter #" + str(number) + ".\nIt automatically reports recently merged features and discussions about the ASPECT mantle convection code.\n\n"
     html_header = """\
     <html>
       <head></head>
       <body>
         <p>Hello everyone!<br><br>
-           This is the biweekly ASPECT newsletter #""" + str(number) + ".<br>It automatically reports recently merged features and discussions about the ASPECT mantle convection code.<br>"
+           This is ASPECT newsletter #""" + str(number) + ".<br>It automatically reports recently merged features and discussions about the ASPECT mantle convection code.<br>"
     
     return html_header,text_header
 
@@ -53,7 +54,7 @@ Additional information can be found at https://aspect.dealii.org/, and https://g
 
 def send_mail(html_mail,text_mail):
     # Now compose message to send:
-    me = "rene.gassmoeller@mailbox.org"
+    me = '"Rene Gassmoeller" <rene.gassmoeller@mailbox.org>'
     you = "aspect-devel@geodynamics.org"
     #you = "r.gassmoeller@mailbox.org"
     
@@ -75,7 +76,8 @@ def send_mail(html_mail,text_mail):
     msg.attach(part2)
     
     # Send the message via SMTP server.
-    s = smtplib.SMTP('mail.math.tamu.edu')
+    s = smtplib.SMTP_SSL('smtp.mailbox.org:465')
+    s.login("r.gassmoeller@mailbox.org",pw)
     
     # sendmail function takes 3 arguments: sender's address, recipient's address
     # and message to send - here it is sent as one string.
@@ -93,24 +95,24 @@ def traverse_prs(issues):
             pr = r.json()
 
             create_time = datetime.datetime.strptime(issue['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-            merge_time = datetime.datetime.now() - datetime.timedelta(60)
-            if pr['merged_at'] != None:
+            merge_time = now - 2 * report_timespan
+            if pr.has_key('merged_at') and pr['merged_at'] != None:
                 merge_time = datetime.datetime.strptime(pr['merged_at'], '%Y-%m-%dT%H:%M:%SZ')
             
             # If created or merged within the last 14 days:
-            if (now - create_time < datetime.timedelta(14)) or (now - merge_time < datetime.timedelta(14)):
-                html_pr = '<a href="' + pr['_links']['html']['href'] + '">#' + str(pr['number']) + '</a>: ' + pr['title'] + ' ('
+            if (now - create_time < report_timespan) or (now - merge_time < report_timespan):
+                html_pr = '<a href="' + pr['_links']['html']['href'] + '">#' + str(pr['number']) + '</a>: ' + unidecode(pr['title']) + ' ('
                 html_pr += 'proposed by ' + '<a href="' + pr['user']['html_url'] + '">' + pr['user']['login'] + '</a>'
 
-                if now - merge_time < datetime.timedelta(14):
+                if now - merge_time < report_timespan:
                     html_pr += '; merged'
  
                 html_pr += ')<br>\n'
 
-                text_pr =  '#' + str(pr['number']) + ': ' + pr['title'] + ' (' 
+                text_pr =  '#' + str(pr['number']) + ': ' + unidecode(pr['title']) + ' (' 
                 text_pr += 'proposed by ' + pr['user']['login'] 
 
-                if now - merge_time < datetime.timedelta(14):
+                if now - merge_time < report_timespan:
                     text_pr += '; merged'
 
                 text_pr +=  ') ' + pr['_links']['html']['href'] + '\n\n'
@@ -125,22 +127,22 @@ def traverse_issues(issues):
     for issue in issues:
         if not issue.has_key('pull_request'):
             create_time = datetime.datetime.strptime(issue['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-            closed_time = datetime.datetime.now() - datetime.timedelta(60)
+            closed_time = now - 2 * report_timespan
             if issue['closed_at'] != None:
                 closed_time = datetime.datetime.strptime(issue['closed_at'], '%Y-%m-%dT%H:%M:%SZ')
     
             # If merged within the last 14 days:
-            if (now - create_time < datetime.timedelta(14)) or (now - closed_time < datetime.timedelta(14)):
+            if (now - create_time < report_timespan) or (now - closed_time < report_timespan):
                 status_change="("
-                if now - create_time < datetime.timedelta(14):
+                if now - create_time < report_timespan:
                     status_change += "opened"
-                    if now - closed_time < datetime.timedelta(14):
+                    if now - closed_time < report_timespan:
                         status_change += " and "
-                if now - closed_time < datetime.timedelta(14):
+                if now - closed_time < report_timespan:
                     status_change += "closed"
                 status_change += ")"
-                html_pr = '<a href="' + issue['html_url'] + '">#' + str(issue['number']) + '</a> ' + issue['title'] + ' ' + status_change + '<br>\n'
-                text_pr =  '#' + str(issue['number']) + ': ' + issue['title'] + ' ' + status_change + ' ' + issue['html_url'] + '\n\n'
+                html_pr = '<a href="' + issue['html_url'] + '">#' + str(issue['number']) + '</a> ' + unidecode(issue['title']) + ' ' + status_change + '<br>\n'
+                text_pr =  '#' + str(issue['number']) + ': ' + unidecode(issue['title']) + ' ' + status_change + ' ' + issue['html_url'] + '\n\n'
                 issues_body_html = issues_body_html + html_pr
                 issues_body_text = issues_body_text + text_pr
     return issues_body_html, issues_body_text
@@ -159,7 +161,7 @@ def handle_pull_requests(request):
     # closed pull requests
     next_request = request
     while next_request.links.has_key('next'):
-        next_request = requests.get(request.links['next']['url'],auth=('token',token))
+        next_request = requests.get(next_request.links['next']['url'],auth=('token',token))
         pull_requests = next_request.json()
         prs_page_html, prs_page_text = traverse_prs(pull_requests)
         pull_requests_body_html += prs_page_html
@@ -179,33 +181,50 @@ def handle_issues(request):
     
     # This handles older pull requests that do not show up on the first page of
     # closed pull requests
-    while request.links.has_key('next'):
-        request = requests.get(request.links['next']['url'],auth=('token',token))
-        issues = request.json()
+    next_request = request
+    while next_request.links.has_key('next'):
+        next_request = requests.get(next_request.links['next']['url'],auth=('token',token))
+        issues = next_request.json()
         issues_page_html, issues_page_text = traverse_issues(issues)
         issues_body_html += issues_page_html
         issues_body_text += issues_page_text
 
     return issues_body_html,issues_body_text
 
-if (int(strftime("%W")) % 2 != 1):
-    sys.exit()
 
+# Set up all necessary data from files and directories
 path = os.path.dirname(os.path.abspath(__file__))
 
-# Access token for Github. Do not distribute although it has no access
+# Access token for Github.
 tokenfile = open(path+"/token","r")
 token = tokenfile.readline()
 # Remove trailing newline character
 token = token[:-1]
 tokenfile.close()
 
+# Access for Mailbox.
+pwfile = open(path+"/pw","r")
+pw = pwfile.readline()
+# Remove trailing newline character
+pw = pw[:-1]
+pwfile.close()
+
+# Current newsletter index.
 numberfile = open(path+"/number","r")
 number = int(numberfile.readline())
 numberfile.close()
 
-now = datetime.datetime.now()
-update_date = now - datetime.timedelta(14)
+# Check time since last newsletter
+datefile = open(path + "/last_send","r")
+last_date = str(datefile.readline())
+datefile.close()
+
+update_date = datetime.datetime.strptime(last_date[:-1],'%Y-%m-%d %H:%M:%S')
+now = datetime.datetime.utcnow()
+report_timespan = now - update_date
+
+if report_timespan < datetime.timedelta(13.5):
+    sys.exit()
 
 # Handle recently closed issues and pull requests
 # Note that all pull requests are also issues in the github API
@@ -243,3 +262,7 @@ if (pull_requests_body_html != "") or (issues_body_html != ""):
     numberfile = open(path+"/number","w")
     numberfile.write(str(number+1))
     numberfile.close()
+
+    datefile = open(path + "/last_send","w")
+    datefile.write(now.strftime('%Y-%m-%d %H:%M:%S'))
+    datefile.close()
